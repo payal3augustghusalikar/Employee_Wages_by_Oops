2c2d366dcac9d9d6fc32e3b6dcb0a33f33ee8478
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-24 19:06:41 +0530

    Loaded files for first time

---
+++ InEmployeeWageOops.java
@@ -0,0 +1,7 @@
+package com.computation;
+
+public interface InEmployeeWageOops {
+	
+		  public void addCompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth);
+		
+}
